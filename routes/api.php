5f6eb8e2daaaf7b8ce1a56f72718a961347f1bbf
H2Vid <dstmpl17@gmail.com>
2025-01-04 07:07:14 +0700

    menmabhi route api untuk orders

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\Userscontroller;
 use App\Http\Controllers\ServiceController;
 
@@ -10,3 +11,4 @@ Route::post ('/users/login', [Userscontroller::class, 'login']);
 Route::get('/users', [Userscontroller::class, 'getUsers']);  // GET untuk mendapatkan semua provider
 Route::get('/users/{id}', [Userscontroller::class, 'getUserById']);  // GET untuk mendapatkan provider berdasarkan ID
 Route::resource('services', ServiceController::class);
+Route::resource('orders', OrderController::class);
